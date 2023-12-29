d318958f3f009aaa7db2cfa9e68966034aa3aecc
ydvsahil03 <ydvsahil0003@gmail.com>
2023-12-29 17:00:52 +0530

    first comm3

--- test/test.py
+++ test/test.py
@@ -1,2 +1,38 @@
-print("Hello World")
-print("Hello")
+# print("Hello World")
+# print("Hello")
+# name = " bankai"
+# lastname="full"
+# print("Hello",name+lastname)
+# print(type(name))
+# # print(name +"senbonzakura")
+
+# age = 21
+# # age+=2
+# print("yout age "+str(age))
+# height = 230.4
+#
+# # print(height, type(height))
+# human = False
+# print("are yout a human "+str(human))
+# print(type(human))
+# name, age, art = "bro", 21, False
+# print(name, age, art)
+# name = "sexy bro"
+# print(len(name))
+# print(name.find("x"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("0"))
+# print(name.replace("o", "a"))
+# print(name*3)
+# typecasting
+x=1
+y=2.3
+y= int(y)
+z="3"
+
+print(x,(y),int(z)*3)
+
