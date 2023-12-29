96dc4cde9293eba2b7ce20cfcb09f4d4143288e5
ydvsahil03 <ydvsahil0003@gmail.com>
2023-12-29 17:24:04 +0530

    first comm3

--- test/test.py
+++ test/test.py
@@ -29,10 +29,37 @@
 # print(name.replace("o", "a"))
 # print(name*3)
 # typecasting
-x=1
-y=2.3
-y= int(y)
-z="3"
-
-print(x,(y),int(z)*3)
-
+# x = 1
+# y = 2.3
+# y = int(y)
+# z = "3"
+#
+# print(x, y, int(z) * 3)
+# name = input("what is your name ")
+# age = int(input("teel your age "))
+# print("hello "+name+" you are "+str(age)+" years")
+# import math
+#
+# pi = 3.142
+# x, y, z = 1, 2, 3
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi, 2))
+# print(math.sqrt(pi))
+# print(max(x, y, z))
+# print(min(x, y, z))
+# slicing
+# name = "lakshya "
+# first_name = name[0:5:1]
+# last_name = name[3:6]
+# funky_name = name[::3]
+# print(first_name)
+# print(last_name)
+# print(funky_name)
+# reversed_name = name[::-1]
+# print(reversed_name)
+# website = "https://www.youtube.com/"
+# rty = slice(7, -4)
+# print(website[rty])
